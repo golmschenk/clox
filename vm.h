025a5a18af0c5a670a88bf42da64c809f1022d50
golmschenk <greg@olmschenk.com>
2025-09-21 10:02:38 -0400

    15

--- vm.h
+++ vm.h
@@ -2,20 +2,29 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "value.h"
 
-typedef struct {
+#define STACK_MAX 256
+
+typedef struct
+{
     Chunk* chunk;
     uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
 } VM;
 
-typedef enum {
+typedef enum
+{
     INTERPRET_OK,
     INTERPRET_COMPILE_ERROR,
     INTERPRET_RUNTIME_ERROR
-  } InterpretResult;
+} InterpretResult;
 
 void initVM();
 void freeVM();
 InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
 
 #endif
